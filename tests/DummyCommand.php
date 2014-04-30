64aa4843ec178c60496d8999899271eeef56ae28
Ilya S <public.ilya.s@gmail.com>
2014-04-30 21:49:38 +0700

    add a test dummy

---
+++ tests/DummyCommand.php
@@ -0,0 +1,20 @@
+<?php namespace Horse\Testing;
+
+use Horse\Command;
+
+class DummyCommand extends Command {
+
+    /**
+     * Run the command.
+     *
+     * @param mixed $input
+     * @param mixed $output
+     * @return void
+     */
+    public function go($input, $output)
+    {
+        // ???
+    }
+
+}
+
