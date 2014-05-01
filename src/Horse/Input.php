98e7178cbb16871837d897e92ad1e8d111b91c78
Ilya S <public.ilya.s@gmail.com>
2014-05-01 15:05:15 +0700

    write some code for Input, can extract to Horse/Proxy?

--- src/Horse/Input.php
+++ src/Horse/Input.php
@@ -1,8 +1,40 @@
 <?php namespace Horse;
 
+use Symfony\Component\Console\Input\InputInterface;
+
 class Input {
 
+    /**
+     * The input.
+     *
+     * @var InputInterface
+     */
+    protected $input;
 
+    /**
+     * The constructor.
+     *
+     * @param InputInterface $input
+     * @return Input
+     */
+    public function __construct(InputInterface $input)
+    {
+        $this->input = $input;
+    }
+
+    // custom methods....
+
+    /**
+     * Redirect dynamic method call.
+     *
+     * @param string $name
+     * @param array $arguments
+     * @return mixed
+     */
+    public function __call($name, array $arguments = [])
+    {
+        return \call_user_func_array([$this->input, $name], $arguments);
+    }
 
 }
 
--- tests/Spec/Horse/InputSpec.php
+++ tests/Spec/Horse/InputSpec.php
@@ -1,8 +1,14 @@
 <?php namespace Spec\Horse;
 
 use PhpSpec\ObjectBehavior;
+use Symfony\Component\Console\Input\ArrayInput;
 
 class InputSpec extends ObjectBehavior {
+
+    function let()
+    {
+        $this->beConstructedWith(new ArrayInput([]));
+    }
 
     function it_can_be_instantiated()
     {
